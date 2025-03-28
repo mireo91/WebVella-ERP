cfba43f8df6a233c0978de9f31b2f61112b6c685
mireo <m.czernik@bh1.pl>
2025-03-28 16:48:50 +0100

    Statecode snippet

---
+++ Reomi.Erp.Plugin.Sale/Snippets/StateCodeOptionsSnippet.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using WebVella.Erp.Api.Models;
+using WebVella.Erp.Web.Models;
+
+
+public class StateCodeOptionsSnippet : ICodeVariable
+{
+	public object? Evaluate(BaseErpPageModel pageModel)
+	{
+		var list = ((SelectField)pageModel.ErpRequestContext.Entity.Fields.Find(x => x is SelectField && x.Name == "statecode")!).Options;
+		List<Entity> stateCodes = new List<Entity>();
+		foreach(var test in list){
+			stateCodes.Add( new Entity(){Label = test.Label, Name = test.Value});
+		}
+		try{
+			return ((SelectField)pageModel.ErpRequestContext.Entity.Fields.Find(x => x is SelectField && x.Name == "statecode")!)?.Options;
+		}
+		catch(Exception ex){
+			return "Error: " +  ex.Message;
+		}
+	}
+}
+
