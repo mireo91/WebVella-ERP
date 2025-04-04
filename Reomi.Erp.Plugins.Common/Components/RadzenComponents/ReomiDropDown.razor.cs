26a13eca878a686b5ed9d75c8d01391af1e99649
mireo <m.czernik@bh1.pl>
2025-04-04 16:42:17 +0200

    DropDown working solution

--- Reomi.Erp.Plugins.Common/Components/RadzenComponents/ReomiDropDown.razor.cs
+++ Reomi.Erp.Plugins.Common/Components/RadzenComponents/ReomiDropDown.razor.cs
@@ -1,8 +1,13 @@
+using CsvHelper.Expressions;
 using Microsoft.AspNetCore.Components;
 using Microsoft.CodeAnalysis.Options;
 using Radzen;
+using Radzen.Blazor;
+using WebVella.Erp.Api;
 using WebVella.Erp.Api.Models;
+using WebVella.Erp.Eql;
 using WebVella.Erp.Web.Components;
+using RecordManager = WebVella.Erp.Api.RecordManager;
 
 namespace Reomi.Erp.Plugins.Common.Components.RadzenComponents;
 
@@ -15,20 +20,40 @@ public partial class ReomiDropDown : ComponentBase
     [Parameter]
     public PcFieldSelect.PcFieldSelectOptions FieldOptions { get; set; }
 
+    private bool _isRequired = false;
+    
+    RadzenTemplateForm<string> _form;
     // [Parameter] public bool IsExpanded { get; set; } = true;
     // [Parameter] public bool IsEditable { get; set; } = true;
     // [Parameter] public string? CustomEqlCommand { get; set; }
     IEnumerable<SelectOption> options;
+    string fieldValue = String.Empty;
 
+    private Entity? _entity = null;
+    private EntityRecord? _record = null;
     protected override void OnInitialized()
     {
         base.OnInitialized();
+        fieldValue = FieldOptions.Value;
+        if (FieldOptions.ConnectedEntityId != null)
+        {
+            var response = (new EntityManager()).ReadEntity((Guid)FieldOptions.ConnectedEntityId!);
+            if( response.Success )
+            {
+                _entity = response.Object;
+                var recordId = "f4d87b41-1fe1-48fe-b091-b2c7e566a8ee";
+                _record = new EqlCommand($"SELECT id, {FieldOptions.Name} FROM {_entity.Name} WHERE id = @id", new EqlParameter("id", recordId)).Execute().FirstOrDefault();
+                if(_record!=null)
+                    fieldValue = _record![FieldOptions.Name]!=null?_record[FieldOptions.Name].ToString()!:"";
+                if (FieldOptions.Name != null) _isRequired = _entity.Fields.Any(c => c.Name == FieldOptions.Name && c.Required);
+            }
+        }
+        // FieldOptions.
         // FieldOptions.ConnectedEntityId
         // Console.WriteLine(Model.AjaxDatasourceApi);
         // Console.WriteLine(Model.AjaxDatasourceApi);
         // Console.WriteLine(Model.AjaxApiUrlDs);
     }
-
     void LoadData(LoadDataArgs args)
     {
         var query = Options;
@@ -39,7 +64,48 @@ public partial class ReomiDropDown : ComponentBase
         }
         
         options = query.ToList();
-        
         InvokeAsync(StateHasChanged);
+    }
+    
+    void OnSubmit(object value)
+    {
+        if (_entity == null) return;
+        if (_record == null) return;
+        _record[FieldOptions.Name] = value;
+        var response = (new RecordManager()).UpdateRecord(_entity?.Name, _record);
+        var message = new NotificationMessage
+        {
+            Severity = NotificationSeverity.Success, Summary = "Success Summary", Detail = response.Message,
+            Duration = 4000
+        };
+        if (!response.Success)
+        {
+            message = new NotificationMessage
+            {
+                Severity = NotificationSeverity.Error, Summary = "Error Summary", Detail = response.Message,
+                Duration = 4000
+            };
+        }
+        
+        NotificationService.Notify(message);
+    }
+    object? _inlineEditablePreviousValue = null;
+
+    private bool _inlineEditable = false;
+    async Task EditRow()
+    {
+        _inlineEditablePreviousValue = fieldValue;
+        _inlineEditable = true;
+    }
+    async Task SaveRow()
+    {
+        _inlineEditable = false;
+        await _form.Submit.InvokeAsync(fieldValue);
+    }
+
+    void CancelEdit()
+    {
+        _inlineEditable = false;
+        fieldValue = _inlineEditablePreviousValue.ToString();
     }
 }
